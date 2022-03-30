9491bd8c17386dd629fe3f506444704f5ab655ac
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-03-30 23:22:34 +0300

    feat/настроить swiper

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,22 @@ import './index.scss'
 import 'normalize.css'
 
 import "../utils/importImages"
+import Swiper, { Navigation, Pagination } from 'swiper'
 
+Swiper.use([Navigation, Pagination]);
+
+new Swiper('.about-us__swiper',{
+        pagination: {
+            el: '.about-us__swiper-pagination',
+            clickable: true,
+            bulletClass: 'about-us__swiper-pagination-bullet',
+            bulletActiveClass: 'about-us__swiper-pagination-bullet_active',
+        },
+        spaceBetween: 10,
+        centeredSlides: true,
+        loop: true,
+        setWrapperSize: true,
+        slidesPerView: 1.1,
+
+    }
+);
