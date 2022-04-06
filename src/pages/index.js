681c07de3ecc733ba2abc925bdf5065016d66382
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-04-06 23:15:05 +0300

    feat: partners mobile version

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,40 @@
-import './index.scss'
+import './index.css'
 import 'normalize.css'
 
 import "../utils/importImages"
 
+import Swiper, { Navigation, Pagination } from 'swiper';
+  // import Swiper and modules styles
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+
+  // init Swiper:
+  const swiper = new Swiper('.swiper', {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination],
+    slidesPerView: 2,
+    spaceBetween: 8,
+    pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        /*breakpoints: {
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        },*/
+  });
