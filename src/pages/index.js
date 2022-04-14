d35988869fe04b3cc6a6432689e87b389fad98d3
ChumachI <94695810+ChumachI@users.noreply.github.com>
2022-04-14 16:46:29 +0300

    итог

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,40 @@ import 'normalize.css'
 import "../utils/importImages"
 import Swiper, { Navigation, Pagination } from 'swiper'
 
+const burgerButton = document.querySelector('.header__burger-menu-button');
+const menu = document.querySelector('.menu');
+const closeMenuButton = menu.querySelector('.menu__close');
+
+
+
+
+
+
+const menuContainers = menu.querySelectorAll('.menu__subitem-container');
+
+menuContainers.forEach(item => {
+    const subitems = item.querySelectorAll('.menu__subitem');
+    item.addEventListener('click', ()=>{
+        subitems.forEach(subitem => {
+            
+            subitem.classList.toggle('menu__subitem_active');
+        })
+    })
+})
+
+
+
+
+
+
+
+burgerButton.addEventListener('click',switchMenu);
+closeMenuButton.addEventListener('click',switchMenu);
+
+function switchMenu(){
+    menu.classList.toggle('menu_open');
+}
+
 Swiper.use([Navigation, Pagination]);
 
 new Swiper('.about-us__swiper',{
@@ -21,3 +55,4 @@ new Swiper('.about-us__swiper',{
 
     }
 );
+
